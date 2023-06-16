2c22bf6af91cc5a8d787bb2c8fced541cdc87859
kevincas89 <89276116+kevincas89@users.noreply.github.com>
2023-06-16 14:19:51 -0500

    boton registrar rol

--- src/pages/admin/gestionar-roles.tsx
+++ src/pages/admin/gestionar-roles.tsx
@@ -62,12 +62,19 @@ export default function (){
                         handleSelectRow={(e)=>console.log(e)} />
                 </div>
 
+
+                <div className="my-10">
+
                 <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline "
                     type="submit"
                 >
-                    Iniciar sesión
+                    Registrar Rol
                 </button>
+
+                </div>
+                
+                
             </div>
         </div>
     )
