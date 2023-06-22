c7dfddf32ec4e80f56bcea2afc8797d1350e7fee
eucogar <eucogar7@gmail.com>
2023-06-21 19:12:06 -0500

    models

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Login } from "../models/Login";
 
-export default function Login() {
+export default function LoginForm() {
+    const { register, handleSubmit } = useForm<Login>();
+    const onSubmit: SubmitHandler<Login> = (data) => {
+        console.dir(data); // Muestra los datos del formulario en la consola
+    };
+
     return (
         <div className="flex justify-center items-center h-screen">
-            <form className="max-w-md bg-white rounded-lg shadow-md p-6">
+            <form
+                className="max-w-md bg-white rounded-lg shadow-md p-6"
+                onSubmit={handleSubmit(onSubmit)}
+            >
                 <h2 className="text-2xl font-bold mb-4">Iniciar sesión</h2>
                 <div className="mb-4">
                     <label
@@ -17,6 +27,7 @@ export default function Login() {
                         id="email"
                         type="email"
                         placeholder="Correo Electrónico"
+                        {...register("email")}
                     />
                 </div>
                 <div className="mb-4">
@@ -31,6 +42,7 @@ export default function Login() {
                         id="password"
                         type="password"
                         placeholder="Contraseña"
+                        {...register("password")}
                     />
                 </div>
                 <button
