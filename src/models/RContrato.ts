bca48e7c2a367a81ed9f1048ea6e3ac136596ad6
kevincas89 <89276116+kevincas89@users.noreply.github.com>
2023-06-21 20:25:13 -0500

    Rcontrato

---
+++ src/models/RContrato.ts
@@ -0,0 +1,13 @@
+export interface RContrato {
+    DependencyName: String
+    SerieName: String
+    SubSerieName: String
+    Asunto: String
+    InitialDate: Int
+    FinalDate: Int
+    Estante: String
+    Bandeja: String
+    Box: String
+    NoDeOrden: String
+    Observations: String
+}
